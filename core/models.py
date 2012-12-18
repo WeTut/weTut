3e04167de7d5b643c2c48c24b54aaf2650b88274
Cyrill <cyrilldurigon@gmail.com>
2012-12-18 11:04:25 +0100

    model users

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Profile (models.Models):
+	slug = models.SlugField(max_length = 200)
+	email = models.EmailField()
+	pictureProfile = models.ImageField
+	city = models.CharField(max_length = 200)
+	state = models.CharField(max_length = 200)
+	urlPortfolio = models.CharField (max_length = 200)
+	urlViadeo = models.CharField (max_length = 200)
+	urlLinkedin = models.CharField (max_length = 200)
+	urlTwitter = models.CharField (max_length = 200)
+	urlFacebook = models.CharField (max_length = 200)
+	points = models.IntegerField()
+	status = models.CharField(max_length = 200)
+
+	def __unicode__(self):
+		return u'%s' % (self.email)
