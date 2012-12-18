a0398bed5677489a30b0485b2ef607e8ccbd69c2
Cyrill <cyrilldurigon@gmail.com>
2012-12-18 12:29:43 +0100

    creation models

--- core/models.py
+++ core/models.py
@@ -1,9 +1,9 @@
 from django.db import models
+from django.auth.user import User
 
 class Profile (models.Models):
-	slug = models.SlugField(max_length = 200)
 	email = models.EmailField()
-	pictureProfile = models.ImageField
+	pictureProfile = models.ImageField()
 	city = models.CharField(max_length = 200)
 	state = models.CharField(max_length = 200)
 	urlPortfolio = models.CharField (max_length = 200)
@@ -16,3 +16,46 @@ class Profile (models.Models):
 
 	def __unicode__(self):
 		return u'%s' % (self.email)
+
+class Answer (models.Models):
+	nbLike = models.IntegerField()
+	text = models.TextField()
+	date = models.DateField()
+
+	user = models.ForeignKey(User)
+
+	def __unicode__(self):
+		return u'%s' % (self.date)
+
+
+class Question (models.Models):
+	title = models.CharField(max_length = 200)
+	picture = models.ImageField()
+	message = models.CharField(max_length = 400)
+
+
+	user = models.ForeignKey(User)
+
+	def __unicode__(self):
+		return u'%s' % (self.title)
+
+class Software(models.Models):
+	name = models.CharField(max_length = 200)
+
+	def __unicode__(self):
+		return u'%s' % (self.name)
+
+
+class Tags(models.Models):
+	name = models.CharField(max_length = 200)
+
+	def __unicode__(self):
+		return u'%s' % (self.name)
+
+class Media(models.Models):
+	link = models.CharField(max_length = 200)
+
+	question = mmodels.ForeignKey("Question")
+
+	def __unicode__(self):
+		return u'%s' % (self.link)
