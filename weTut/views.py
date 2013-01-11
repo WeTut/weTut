4d36316d531e3243368d7ff2ad9163991b75d597
Jeremy <jeremybardeau@free.fr>
2013-01-11 14:28:55 +0100

    ok

--- weTut/views.py
+++ weTut/views.py
@@ -31,7 +31,7 @@ def login_view(request):
 			if user is not None:
 				if user.is_active:
 					login(request,user)
-					return HttpResponseRedirect('/home/') # Redirect after POST
+			return HttpResponseRedirect('/home/') # Redirect after POST
 
 		else:
 			form = AuthForm() # An unbound form
