93c374e94da9345145e3fd3991b21d3dba0b4100
julienhennet <julien.hennet@gmail.com>
2013-03-07 22:03:38 +0100

    others bugs on profile page

--- members/views.py
+++ members/views.py
@@ -12,8 +12,7 @@ from tutorials.models import Question, FollowQuestion
 
 def profile(request):
 	profile = Profile.objects.get(user=request.user)
-	profileform = ProfileForm(instance=profile)
-	
+
 	if request.method == 'POST': # If the form has been submitted...
 		if request.POST['submit'] == 'profileSubmit':#Si une reponse a ete envoyee
 			form = ProfileForm(request.POST, request.FILES) # A form bound to the POST data
@@ -23,15 +22,21 @@ def profile(request):
 					post.avatar = profile.avatar
 				post.user = request.user
 				post.email = profile.email
-				post.points = 0
+				post.points = profile.points
 				post.status = profile.status
+				post.views = profile.views
+				post.nb_likes = profile.nb_likes
+				post.nb_questions = profile.nb_questions
 				profile.delete()
-				post.save()				
+				post.save()
+				profile = Profile.objects.get(user=request.user)
 
 	followedquestions = []
 	follows = FollowQuestion.objects.filter(user=request.user) 
 	for follow in follows:
-		followedquestions.append ( get_object_or_404(Question, id=follow.question.id) )	
+		followedquestions.append ( get_object_or_404(Question, id=follow.question.id) )
+
+	profileform = ProfileForm(instance=profile) # An unbound form
 	
 	return render_to_response('members/profile.html', {'profileform': profileform, 'followedquestions':followedquestions }, context_instance=RequestContext(request))
 	
