b754b7059f5dd0941e99c079535cd47e6fe6411d
Jeremy <jeremybardeau@free.fr>
2013-03-27 17:26:35 +0100

    .

--- tutorials/forms.py
+++ tutorials/forms.py
@@ -3,6 +3,13 @@ from models import Question, Answer, CommentAnswer, Media
 from django.forms import ModelForm
 
 class QuestionForm(ModelForm):
+	def __init__(self, *args, **kwargs):
+		super(QuestionForm, self).__init__(*args, **kwargs)
+		self.fields['media1'].required = False
+		self.fields['media2'].required = False
+		self.fields['media3'].required = False
+		self.fields['video'].required = False
+
 	class Meta:
 		model = Question
 		exclude=['slug', 'user', 'views', 'date', 'answers','tag1','tag2','tag3', 'currentUserFollows', 'currentUserLikes','validate']
@@ -33,7 +40,7 @@ class FilterForm(forms.Form):
     
     filter = forms.ChoiceField(choices=FILTER_CHOICES, widget=forms.Select())
 
-#class MediaForm(ModelForm):
-#	class Model:
-#		model = Media
-#		exclude=['question_id']
+class MediaForm(ModelForm):
+	class Meta:
+		model = Media
+		exclude=['question_id']
